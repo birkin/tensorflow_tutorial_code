acaf2a88c6ec8b034591489b6a483546d43ffa6d
birkin <birkin.diana@gmail.com>
2017-11-17 11:28:59 -0500

    adds section; updates to py3 syntax

--- scratch.py
+++ scratch.py
@@ -10,3 +10,16 @@ from keras.layers import Dropout # dropout a method used to penalize for overfit
 
 # loading the housing data stored as a numpy file into training, validation and testing matricies
 (x_train, y_train), (x_val, y_val), (x_test, y_test) = np.load( './housing.npy', encoding='bytes' )
+
+print ("Shape of your training matrix and labels:")
+print (np.shape(x_train)) # (350, 13): 350 rows/samples and 13 columns/features
+print (np.shape(y_train)) # (350,): 350 labels correspoding to the training features
+print(' ')
+print ("Shape of your validation matrix and labels:")
+print (np.shape(x_val)) # (50, 13): 50 rows/samples and 13 columns/features
+print (np.shape(y_val)) # (50,): 50 labels correspoding to the validation features
+print(' ')
+print ("Shape of your test matrix and labels:")
+print (np.shape(x_test)) # (106, 13): 106 rows/samples and 13 columns/features
+print (np.shape(y_test)) # (106,): 106 labels correspoding to the test features
+print(' ')
